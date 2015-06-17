d24aeebcd33d09d5ef96c0384d5ac79bedfd146a
kkulikowski <kontakt@internetowakuznia.pl>
2015-06-17 20:37:31 +0200

    Fixed Product Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :title, :description, :price
+  validates_format_of :price, :with => /\A\d+(?:\.\d{0,2})?\z/
+
+  def average_rating
+    self.reviews.average(:rating).round(2) if self.reviews.size > 0
+  end
 end
